0a6f94de12b59eea060fc88fb42bf2942e0c9695
Mckinsley Apollon <mckinsley@hotmail.com>
2020-09-10 07:08:44 +0000

    Lecture de fichier fonctionnel. Reste juste a implimenter l'ecriture dans le fichier final.

--- main.cpp
+++ main.cpp
@@ -2,12 +2,40 @@
 #include <fstream>
 #include <stack>
 #include "Score.hpp"
-
 using namespace std;
+
+stack<Score> sortStack(stack<Score> &input) 
+{ 
+    stack<Score> tmpStack; 
+  
+    while (!input.empty()) 
+    { 
+        // pop out the first element 
+        Score tmp = input.top(); 
+        input.pop(); 
+  
+        // while temporary stack is not empty and top 
+        // of stack is greater than temp 
+        while (!tmpStack.empty() && tmpStack.top().getHighScore() > tmp.getHighScore()) 
+        { 
+            // pop from temporary stack and push 
+            // it to the input stack 
+            input.push(tmpStack.top()); 
+            tmpStack.pop(); 
+        } 
+  
+        // push temp in tempory of stack 
+        tmpStack.push(tmp); 
+    } 
+  
+    return tmpStack; 
+} 
+
+
 int main() {
   stack<Score> scores;
 
-  ifstream lecteur("asterostorm.txt");
+  ifstream lecteur("highscore.txt");
     
     while (lecteur.eof() == false) {
         if(!lecteur.eof()) {
@@ -22,18 +50,24 @@ int main() {
 
             Score score = Score(login, identifiantPartie, highScore);
 
-            scores.push(score);
-            
-            
+            scores.push(score);     
         }
     }
 
     lecteur.close();
 
-    for(score : scores) {
-      cout << scores.top().getLogin() << " " << scores.top().getIdentifiantPartie() << " = " << scores.top().getHighScore() << endl;
+    stack<Score> scoresSorted = sortStack(scores);
 
-      scores.pop();
+    while(!scoresSorted.empty()) {
+      ofstream fichier;
+
+      fichier.open("asterostorm.txt");
+      fichier << scoresSorted.top().getLogin() << endl;
+      fichier << scoresSorted.top().getIdentifiantPartie() << endl;
+      fichier << "Asterostorm=" << scoresSorted.top().getHighScore() << endl;
+      scoresSorted.pop();
+      
+      fichier.close();
     }
     
     
